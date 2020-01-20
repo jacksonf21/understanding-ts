acedbd43a70d46498db1e3a81c682fa74408d9a4
jacksonf21 <jacksonf21@hotmail.com>
2020-01-20 12:06:22 -0500

    interface notes

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
-function test() {
-  const max = 'max';
+// interface PersonTwo {
+//   name: string;
+//   age: number;
+  
+//   greet(phrase: string): void;
+// }
+
+// let user1: PersonTwo;
+
+// user1 = {
+//   name: 'joe',
+//   age: 30,
+//   greet(phrase: string) {
+//     console.log(phrase + this.name);
+//   }
+// };
+
+// user1.greet('hi there i\'m max');
+
+interface Greetable {
+  name: string;
+
+  greet(phrase: string): void;
 };
+
+class PersonTwo implements Greetable {
+  constructor(public name: string) {}
+  
+  greet(phrase: string) {
+    console.log(phrase);
+  }
+};
+
+let user1 = new PersonTwo('Hello!');
